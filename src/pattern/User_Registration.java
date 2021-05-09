5fdb47c7255a1023ac2c667632f8178d49dea542
sanketb412 <sanketbagde38@gmail.com>
2021-05-09 22:27:28 +0530

    Added: code of regex for valid last name

--- src/pattern/User_Registration.java
+++ src/pattern/User_Registration.java
@@ -9,15 +9,26 @@ public class User_Registration {
 		// TODO Auto-generated method stub
 		System.out.println("..###..Welcome to User Registration program..###..");
 		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the First Name for email to Check");
-		String email = sc.nextLine();
-		boolean x = Pattern.matches("^([A-Z]{1}[a-zA-Z]{2,})$", email);
+		Scanner fn = new Scanner(System.in);
+		System.out.println("Enter the First Name to Check");
+		String firstname = fn.nextLine();
+		boolean x = Pattern.matches("^([A-Z]{1}[a-zA-Z]{2,})$", firstname);
 		if (x == true) {
-			System.out.println("Valid Name");
+			System.out.println("Valid Name\n");
 		} else {
-			System.out.println("Invalid Name");
+			System.out.println("Invalid Name\n");
 		}
+		@SuppressWarnings("resource")
+		Scanner ln = new Scanner(System.in);
+		System.out.println("Enter the Last Name to Check");
+		String lastname = ln.nextLine();
+		boolean y = Pattern.matches("^([A-Z]{1}[a-zA-Z]{2,})$", lastname);
+		if (y == true) {
+			System.out.println("Valid Name\n");
+		} else {
+			System.out.println("Invalid Name\n");
+		}
+		
 	}
 
 }
