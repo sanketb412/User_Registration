e97ba1b20465f2893dd9a6969624f39203ad3235
sanketb412 <sanketbagde38@gmail.com>
2021-05-09 22:11:55 +0530

    Added: code of Welcomming

---
+++ src/pattern/User_Registration.java
@@ -0,0 +1,10 @@
+package pattern;
+
+public class User_Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("..###..Welcome to User Registration program..###..");
+	}
+
+}
