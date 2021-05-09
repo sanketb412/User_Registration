76546b74d5b2dae0672c9a3cb2b6c4d6bbe435bf
sanketb412 <sanketbagde38@gmail.com>
2021-05-09 22:15:58 +0530

    Added: code of regex for valid first name

--- src/pattern/User_Registration.java
+++ src/pattern/User_Registration.java
@@ -1,10 +1,23 @@
 package pattern;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class User_Registration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("..###..Welcome to User Registration program..###..");
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First Name for email to Check");
+		String email = sc.nextLine();
+		boolean x = Pattern.matches("^([A-Z]{1}[a-zA-Z]{2,})$", email);
+		if (x == true) {
+			System.out.println("Valid Name");
+		} else {
+			System.out.println("Invalid Name");
+		}
 	}
 
 }
